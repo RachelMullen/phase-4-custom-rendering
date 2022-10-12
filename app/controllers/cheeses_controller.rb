cd31a6d2a10ac571cb54856d1a9575652e1584e8
Rachel Mullen <mullen.rachel@gmail.com>
2022-10-12 14:57:27 -0600

    commit tree 62802a664c880033cbe0ac0635dd9b9c77daa714

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -6,10 +6,24 @@ class CheesesController < ApplicationController
     render json: cheeses
   end
 
+  def index  
+    cheeses = Cheese.all
+    render json: cheeses, only: [:id, :name, :price, :is_best_seller]
+  end
   # GET /cheeses/:id
   def show
     cheese = Cheese.find_by(id: params[:id])
     render json: cheese
+  end
+
+  def show
+    cheese = Cheese.find_by(id: params[:id])
+    render json: {
+      id: cheese.id,
+      name: cheese.name,
+      price: cheese.price,
+      is_best_seller: cheese.is_best_seller
+    }
   end
 
 end
